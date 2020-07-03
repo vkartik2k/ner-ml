b264eac8dd8532d8e6a1e9b12c8afc4d3109b630
vkartik2k <vkartik2k@gmail.com>
2020-07-03 13:00:59 +0530

    s

---
+++ ml/Tweets/tweet_tokeniser.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import re
+from nltk import word_tokenize
+
+list1 =open("covid_tweets.tsv").read().split('\n')
+sentences = []
+
+for i in list1 :
+    print(word_tokenize(i))
+    
+
+
+# charset = " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,-_()[]{}!?:;#'\"/\\%$`&=*+@^~|"
+
+# for i in list1 :
+#     sentences += i.split('    ')
+
+# ans = []
+# for sentence in sentences:
+#     newSentence = ""
+#     for i in sentence :
+#         if i in charset :
+#             newSentence += i
+#     ans.append(newSentence)
+
+# sentences = ans
+
+# arr = []
+# for sentence in sentences:
+#     temp = sentence.split()
+#     temp2 = []
+#     for i in temp:
+#         j = i.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
+#         j = j.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
+#         j = j.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
+#         j = j.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
+#         j = re.split('; |, |- |\? |: |\'',j)
+#         for k in j:
+#             ert = k
+#             k = ""
+#             for s in ert:
+#                 if s=='.' :
+#                     k += ' . '
+#                 else :
+#                     k += s
+
+#             if k != "":
+#                 temp2.append(k)
+
+#     arr.append(' '.join(temp2))
+
+# sentences = arr
+
+# print(sentences)
