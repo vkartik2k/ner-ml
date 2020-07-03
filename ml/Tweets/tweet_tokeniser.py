39b4fd755375f496eecf6c5a88477d0df641ea6e
vkartik2k <vkartik2k@gmail.com>
2020-07-03 14:58:57 +0530

    filtering data

--- ml/Tweets/tweet_tokeniser.py
+++ ml/Tweets/tweet_tokeniser.py
@@ -5,9 +5,6 @@ import nltk
 
 list1 =open("covid_tweets.tsv").read().split('\n')
 sentences = []
-
-for i in list1 :
-    print(nltk.word_tokenize(i))
     
 charset = " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,-_()[]{}!?:;#'\"/\\%$`&=*+@^~|"
 
@@ -26,34 +23,6 @@ sentences = ans
 
 final = []
 for sentence in sentences :
-    final.append(nltk.word_tokenize(i))
-
+    final.append(' '.join(nltk.word_tokenize(sentence)))
 
 print(final)
-# arr = []
-# for sentence in sentences:
-#     temp = sentence.split()
-#     temp2 = []
-#     for i in temp:
-#         j = i.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
-#         j = j.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
-#         j = j.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
-#         j = j.strip('\'').strip('\"').strip('#').strip('(').strip(')').strip('@').strip('.').strip(',').strip('?')
-#         j = re.split('; |, |- |\? |: |\'',j)
-#         for k in j:
-#             ert = k
-#             k = ""
-#             for s in ert:
-#                 if s=='.' :
-#                     k += ' . '
-#                 else :
-#                     k += s
-
-#             if k != "":
-#                 temp2.append(k)
-
-#     arr.append(' '.join(temp2))
-
-# sentences = arr
-
-# print(sentences)
