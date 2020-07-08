68514f5c2da046c4dc1b01bc29666f911a5beec9
Kartik <vkartik2k@gmail.com>
2020-07-08 18:37:14 +0530

    some changes

---
+++ Data/remove.py
@@ -0,0 +1,33 @@
+import csv
+
+f = open("file.csv", "r")
+curr = []
+finalSentences = []
+for x in f:
+    if "Sentence #" in x:
+        if curr != [] :
+            finalSentences.append(curr)
+        curr = []
+    else :
+        y = x.split(',')
+        curr.append(y)
+
+col1 = []
+col2 = []
+
+count = 0
+for i in finalSentences:
+    col1.append('Sentence #'+ str(count))
+    col2.append('')
+    count +=1
+    for j in i:
+        if len(j) == 2:
+            col1.append(j[0])
+            col2.append(j[1][:-1])
+
+with open('acc_out.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for i in range(len(col1)) :
+        writer.writerow([col1[i], col2[i]])
+
+print('DONE')
