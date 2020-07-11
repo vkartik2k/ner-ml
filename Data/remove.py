719d17134b75a79c1855215bc1c074707f38e658
Kartik <vkartik2k@gmail.com>
2020-07-11 14:33:33 +0530

    final data

--- Data/remove.py
+++ Data/remove.py
@@ -1,5 +1,9 @@
 import csv
 
+col1 = []
+col2 = []
+count = 0
+
 f = open("Final/covid.csv", "r")
 curr = []
 finalSentences = []
@@ -12,10 +16,69 @@ for x in f:
         y = x.split(',')
         curr.append(y)
 
-col1 = []
-col2 = []
+for i in finalSentences:
+    col1.append('Sentence #'+ str(count))
+    col2.append('')
+    count +=1
+    for j in i:
+        if len(j) == 2:
+            col1.append(j[0])
+            col2.append(j[1][:-1])
 
-count = 0
+f = open("Final/accident.csv", "r")
+curr = []
+finalSentences = []
+for x in f:
+    if "Sentence #" in x:
+        if curr != [] :
+            finalSentences.append(curr)
+        curr = []
+    else :
+        y = x.split(',')
+        curr.append(y)
+
+for i in finalSentences:
+    col1.append('Sentence #'+ str(count))
+    col2.append('')
+    count +=1
+    for j in i:
+        if len(j) == 2:
+            col1.append(j[0])
+            col2.append(j[1][:-1])
+
+f = open("Final/gali.csv", "r")
+curr = []
+finalSentences = []
+for x in f:
+    if "Sentence #" in x:
+        if curr != [] :
+            finalSentences.append(curr)
+        curr = []
+    else :
+        y = x.split(',')
+        curr.append(y)
+
+for i in finalSentences:
+    col1.append('Sentence #'+ str(count))
+    col2.append('')
+    count +=1
+    for j in i:
+        if len(j) == 2:
+            col1.append(j[0])
+            col2.append(j[1][:-1])
+
+f = open("Final/gali-2.csv", "r")
+curr = []
+finalSentences = []
+for x in f:
+    if "Sentence #" in x:
+        if curr != [] :
+            finalSentences.append(curr)
+        curr = []
+    else :
+        y = x.split(',')
+        curr.append(y)
+
 for i in finalSentences:
     col1.append('Sentence #'+ str(count))
     col2.append('')
